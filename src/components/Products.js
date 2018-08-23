5566dff16a48ffc7fb99ea59ce6a03206300d6c9
quanghung97 <hungcan1997@gmail.com>
2018-08-23 22:15:03 +0700

    all actions in ProductsContainer

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,30 +3,18 @@ import Product from './Product';
 
 class Products extends Component {
     render() {
-        var { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
 
-                    { this.showProducts(products) }
+                    { this.props.children }
 
                 </div>
             </section>
         );
     }
 
-    //index all products
-    showProducts(products) {
-        var result = null;
-        if (products.length > 0) {
-            result = products.map((product, index) => {
-                //add props for each product
-                return <Product key = {index} product = {product}/>
-            });
-        }
-        return result;
-    }
 }
 
 export default Products;
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-// redux store - ProductsContainer - Products(reactjs)// trung gian ket noi  
+// redux store - ProductsContainer - Products(reactjs)// trung gian ket noi
 import Products from './../components/Products';
+import Product from './../components/Product';
 
 class ProductsContainer extends Component {
     render() {
         var { products } = this.props;
         return (
-            <Products products = { products }/>
+            <Products>
+                { this.showProducts(products) }
+            </Products>
         );
+    }
+
+    //index all products
+    showProducts(products) {
+        var result = null;
+        if (products.length > 0) {
+            result = products.map((product, index) => {
+                //add props for each product
+                return <Product key = {index} product = {product}/>
+            });
+        }
+        return result;
     }
 }
 
