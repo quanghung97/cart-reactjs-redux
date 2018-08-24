8b362c371207b70d934f89c7c713f50c00359191
quanghung97 <hungcan1997@gmail.com>
2018-08-24 21:13:22 +0700

    form need state contructor, change quantity should get state in store

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,16 +2,9 @@ import React, {Component} from 'react';
 import * as Message from './../constants/Message';
 class CartItem extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            quantity : 1
-        }
-    }
-
     render() {
         var { item } = this.props;
-        var { quantity } = item.quantity > 0 ? item : this.state;
+        var { quantity } = item;
         return (
             <tr>
                 <th scope="row">
@@ -49,9 +42,6 @@ class CartItem extends Component {
     onUpdateQuantity = (product, quantity) => {
         if (quantity > 0) {
             var { onUpdateProductInCart, onChangeMessage } = this.props;
-            this.setState({
-                quantity : quantity
-            });
             onUpdateProductInCart(product, quantity);
             onChangeMessage(Message.MSG_UPDATE_CART_SUCCESS);
         }
