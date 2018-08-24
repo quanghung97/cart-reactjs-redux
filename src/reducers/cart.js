2a3f4c4ea935bad17b30dc371a73bce1deb094a4
quanghung97 <hungcan1997@gmail.com>
2018-08-24 15:17:35 +0700

    handling actions actAddtoCart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,41 +1,40 @@
 import * as types from './../constants/ActionTypes';
 var data = JSON.parse(localStorage.getItem('CART'));
 
-var initialState = [
-    {
-        product : {
-            id : 1,
-            name : 'Iphone 7 plus',
-            image : 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
-            description : 'Sản phẩm do apple sản xuất',
-            price : 500,
-            inventory : 10,
-            rating : 4
-        },
-        quantity : 5
-    },
-    {
-        product : {
-            id : 3,
-            name : 'Xiaomi 7 plus',
-            image : 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
-            description : 'Sản phẩm do apple sản xuất',
-            price : 450,
-            inventory : 5,
-            rating : 5
-        },
-        quantity : 3
-    }
-];
+var initialState = data ? data : [];
 
 const cart = (state = initialState, action) => {
+    var { product, quantity } = action;
+    var index = -1;
     switch (action.type) {
         case types.ADD_TO_CART:
-            console.log(action);
+            index = findProductInCart(state, product);
+            if (index !== -1) {
+                state[index].quantity += quantity;
+            } else {
+                state.push({
+                    product,
+                    quantity
+                });
+            }
+            localStorage.setItem('CART', JSON.stringify(state));
             return [...state];
         default: return [...state];
 
     }
+}
+
+var findProductInCart = (cart, product) => {
+    var index = -1;
+    if (cart.length > 0) {
+        for (var i = 0; i < cart.length; i++) {
+            if (cart[i].product.id === product.id){
+                index = i;
+                break;
+            }
+        }
+    }
+    return index;
 }
 
 export default cart;
